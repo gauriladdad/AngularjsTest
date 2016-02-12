da9b4445a707b221f2a86918ec0f69e004a35ca9
Gauri Gattani <gauri.gattani@massiveinteractive.com>
2016-02-12 23:31:23 +0000

    format the config files content

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,16 +12,13 @@ module.exports = function(grunt) {
 				background: true
 			}
     },
-
     protractor: {
 	    options: {
-	      configFile: "test/protractor-conf.js", // Default config file
-	      // keepAlive: true, // If false, the grunt process stops when the test fails.
-	      noColor: false, // If true, protractor will not use colors in its output.
-	      // debug: true,
-	      args: {
-
-	      }
+			configFile: "test/protractor-conf.js", 
+			noColor: false, // If true, protractor will not use colors in its output.
+			// debug: true,
+			// keepAlive: true, // If false, the grunt process stops when the test fails.
+	      args: { }
 	    },
 	    endToend: {
 	    	options: {
@@ -33,32 +30,28 @@ module.exports = function(grunt) {
 		    	keepAlive: true
 		    }
 	    }
-	  },
-
-		watch: {
-      options: {
-      	livereload: true
-      },
-      karma: {
-        files: ['js/**/*.js', 'test/unit/*.js'],
-        tasks: ['karma:continuous:run']
-      }
-      ,
-      protractor: {
-        files: ['js/**/*.js', 'test/endToend/*.js'],
-        tasks: ['protractor:continuous']
-      }
+	},	
+	watch: {
+		options: {
+			livereload: true
+		},
+		karma: {
+			files: ['js/**/*.js', 'test/unit/*.js'],
+			tasks: ['karma:continuous:run']
+		},
+		protractor: {
+			files: ['js/**/*.js', 'test/endToend/*.js'],
+			tasks: ['protractor:continuous']
+		}
   	},
-
   	run: {
 	    mock_server: {
-	      options: {
-	        wait: false
-	      },
-	      args: []
+			options: {
+				wait: false
+			},
+			args: []
 	    }
-	  },
-
+	},
 	connect: {
     	options: {
 			port: 9000,
@@ -75,18 +68,7 @@ module.exports = function(grunt) {
 			options: {
 				base: ['']
 			}
-		},
-		runtime: {
-            options: {
-                middleware: function (connect) {
-                    return [
-                        lrSnippet,
-                        mountFolder(connect, 'instrumented'),
-                        mountFolder(connect, '.......')
-                    ];
-                }
-            }
-        }
+		}
     },
 	instrument: {
         files: ['js/*.js', 'js/**/*.js'],
@@ -125,12 +107,6 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-karma');
 	grunt.loadNpmTasks('grunt-protractor-runner');
 	grunt.loadNpmTasks('grunt-run');
-
-	grunt.registerTask('serve', ['karma:unit:start', 'run:mock_server', 'connect:livereload:start']);
-  
-	grunt.registerTask('unit-test', ['karma:unit:start']);
-	
-	grunt.registerTask('endToend-test', ['connect:test',  'protractor:continuous']);
 
 	grunt.registerTask('test', ['karma:unit:start', 'connect:test', 'run:mock_server','protractor:endToend']);
 
--- test/karma-conf.js
+++ test/karma-conf.js
@@ -1,54 +1,53 @@
 module.exports = function(config){
-  config.set({
+	config.set({
     //  root path location that will be used to resolve all relative paths in files and exclude sections
-    basePath : '../',
+		basePath : '../',
 
-    // files to include, ordered by dependencies
-    files : [
-      // include relevant Angular files and libs
-      'node_modules/angular/angular.js',
-      'node_modules/angular-mocks/angular-mocks.js',
-      // include JS files
-      'js/**/*.js',
-      'js/app.js',
-      // include unit test specs
-      'test/unit/*.js'
-    ], 
-    // files to exclude
-    exclude : [
-       'node_modules/angular/*.min.js'
-      ],
+		// files to include, ordered by dependencies
+		files : [
+			  // include relevant Angular files and libs
+			  'node_modules/angular/angular.js',
+			  'node_modules/angular-mocks/angular-mocks.js',
+			  // include JS files
+			  'js/**/*.js',
+			  'js/app.js',
+			  // include unit test specs
+			  'test/unit/*.js'
+		], 
+		// files to exclude
+		exclude : ['node_modules/angular/*.min.js'],
 
-    // karma has its own autoWatch feature but Grunt watch can also do this
-    autoWatch : false,
+		// karma has its own autoWatch feature but Grunt watch can also do this
+		autoWatch : false,
 
-    // testing framework, be sure to install the correct karma plugin
-    frameworks: ['jasmine'],
+		// testing framework, be sure to install the correct karma plugin
+		frameworks: ['jasmine'],
 
-    // browsers to test against, be sure to install the correct browser launcher plugins
-    browsers : ['Chrome'],
+		// browsers to test against, be sure to install the correct browser launcher plugins
+		browsers : ['Chrome'],
 
-    // map of preprocessors that is used mostly for plugins
-    preprocessors: {
-      // test coverage
-      'js/controllers/*.js': ['coverage'],
-      'js/directives/*.js': ['coverage'],
-      'js/app.js': ['coverage']
-    },
+		// map of preprocessors that is used mostly for plugins
+		preprocessors: {
+		  // test coverage
+			'js/controllers/*.js': ['coverage'],
+			'js/directives/*.js': ['coverage'],
+			'js/app.js': ['coverage']
+		},
 
-    reporters: ['progress', 'coverage', 'jenkins'],
+		reporters: ['progress', 'coverage', 'jenkins'],
 
-    // list of karma plugins
-    plugins : [
-      'karma-coverage',
-      'karma-chrome-launcher',
-      'karma-jasmine',
-	  'karma-jenkins-reporter'
-    ],
-	jenkinsReporter: {
-		outputFile: 'test/reports/test-client-report-' + Date.now() + '.xml',
-		suite: 'warehouse-web',
-		classnameSuffix: 'browser-test'
-	}
-})}
+		// list of karma plugins
+		plugins : [
+		  'karma-coverage',
+		  'karma-chrome-launcher',
+		  'karma-jasmine',
+		  'karma-jenkins-reporter'
+		],
+		jenkinsReporter: {
+			outputFile: 'test/reports/test-client-report-' + Date.now() + '.xml',
+			suite: 'warehouse-web',
+			classnameSuffix: 'browser-test'
+		}
+	})
+}
 
